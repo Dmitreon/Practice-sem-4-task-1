ec425234f050070106416b2921479f5c95d5a4a8
Techun <n.gashkin@gmail.com>
2024-05-06 13:41:15 +0300

    debugging

--- src/main/java/pattern/ehu/task1/service/TriangleDataValidatorService.java
+++ src/main/java/pattern/ehu/task1/service/TriangleDataValidatorService.java
@@ -4,14 +4,17 @@ import pattern.ehu.task1.exception.InvalidTriangleFormatException;
 
 public class TriangleDataValidatorService {
 
+    private static final String POINT_PATTERN = "\\s*\\d+(\\.\\d+)?,\\s*\\d+(\\.\\d+)?\\s*";
+    private static final String LINE_DELIMITER = ";\\s*";
+
     public static void validateTriangleData(String triangleData) throws InvalidTriangleFormatException {
-        String[] points = triangleData.split(";\\s*");
+        String[] points = triangleData.split(LINE_DELIMITER);
         if (points.length != 3) {
             throw new InvalidTriangleFormatException("Triangle must have 3 points, found: " + points.length);
         }
 
         for (String point : points) {
-            if (!point.matches("\\s*\\d+(\\.\\d+)?,\\s*\\d+(\\.\\d+)?\\s*")) {
+            if (!point.matches(POINT_PATTERN)) {
                 throw new InvalidTriangleFormatException("Point does not match expected format: " + point);
             }
         }
